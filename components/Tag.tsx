7ec90a7eb2457eccdd096582c82450575065d90c
ArcusTen <moharisofficial@gmail.com>
2025-05-13 19:36:22 +0500

    Updated all files to Strangek Blogsite

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -8,7 +8,7 @@ const Tag = ({ text }: Props) => {
   return (
     <Link
       href={`/tags/${slug(text)}`}
-      className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-red-600 dark:hover:text-red-600"
+      className="mr-3 text-sm font-medium uppercase text-primary-600 hover:text-purple-400 dark:hover:text-purple-400"
     >
       {text.split(' ').join('-')}
     </Link>
